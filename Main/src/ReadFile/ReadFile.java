62fb703e03304a997004bf0e2186314760f0847d
KW <kamilwitek12@o2.pl>
2020-05-18 16:30:58 +0200

    Wypisuje i sprawdza, naprawione znowu

--- Main/src/ReadFile/ReadFile.java
+++ Main/src/ReadFile/ReadFile.java
@@ -5,14 +5,18 @@ import java.nio.file.Paths;
 
 public class ReadFile {
     private static int pointer = 0;
+    private static int limiter = 5;
+    private static int starter = 1;
     public void importFile(){
         try {
             String line = Files.readAllLines(Paths.get("Main\\src\\Questions\\questions.txt")).get(pointer);
             System.out.println(line);
-            for(pointer = 1; pointer < 5; pointer++) {
+            for(pointer = starter; pointer < limiter; pointer++) {
                 line = Files.readAllLines(Paths.get("Main\\src\\Questions\\questions.txt")).get(pointer);
                 System.out.println(line);
             }
+            starter += 6;
+            limiter += 6;
             pointer++;
         } catch (Exception e) {
             e.printStackTrace();
