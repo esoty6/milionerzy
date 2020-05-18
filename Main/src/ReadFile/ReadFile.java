dffdde6b582904b081dd15000ff62989164859e3
KW <kamilwitek12@o2.pl>
2020-05-18 16:15:04 +0200

    Wypisuje i sprawdza

--- Main/src/ReadFile/ReadFile.java
+++ Main/src/ReadFile/ReadFile.java
@@ -6,9 +6,8 @@ import java.nio.file.Paths;
 public class ReadFile {
     int pointer = 0;
     public void importFile(){
-        String line;
         try {
-            line = Files.readAllLines(Paths.get("D:\\Studia\\Projekty JAVA\\milionerzy\\Main\\src\\Questions\\questions.txt")).get(pointer);
+            String line = Files.readAllLines(Paths.get("D:\\Studia\\Projekty JAVA\\milionerzy\\Main\\src\\Questions\\questions.txt")).get(pointer);
             System.out.println(line);
             for(pointer = 1; pointer < line.length(); pointer++) {
                 line = Files.readAllLines(Paths.get("D:\\Studia\\Projekty JAVA\\milionerzy\\Main\\src\\Questions\\questions.txt")).get(pointer);
